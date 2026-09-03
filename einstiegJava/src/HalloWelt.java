b002fff637550cb31ba8e31d7e7c4df991c37a5a
phillip112 <klappergeste@gmail.com>
2026-09-03 11:20:47 +0200

    Rechnen.java

---
+++ einstiegJava/src/HalloWelt.java
@@ -0,0 +1,19 @@
+public class HalloWelt
+{
+    public static void main(String[] args) {
+        System.out.println("Hello and welcome!");
+
+        System.out.println("i = " + 1);
+        System.out.println("jona ist ein loty");
+        System.out.println("i = " + 2);
+        System.out.println("jona ist ein loty");
+        System.out.println("i = " + 3);
+        System.out.println("jona ist ein loty");
+        System.out.println("i = " + 4);
+        System.out.println("Jona ist ein Loty");
+        System.out.println("i = " + 5);
+        System.out.println("jona ist ein loty");
+    }
+}
+
+
---
+++ einstiegJava/src/Rechnen.java
@@ -0,0 +1,18 @@
+public class Rechnen {
+    public static void main(String[] args) {
+        double zahl1 = 5;
+        double zahl2 = 4;
+
+        double ergebnisPlus = zahl1 + zahl2;
+        System.out.println("summe =" + ergebnisPlus);
+
+        double ergebnisMinus = zahl1 - zahl2;
+        System.out.println("differenz =" + ergebnisMinus);
+
+        double ergebnisMal = zahl1 * zahl2;
+        System.out.println("Produkt =" + ergebnisMal);
+
+        double ergebnisGeteilt = zahl1 / zahl2;
+        System.out.println("Quotienten =" + zahl1 + " / " + zahl2 + " = " + ergebnisGeteilt);
+    }
+}
