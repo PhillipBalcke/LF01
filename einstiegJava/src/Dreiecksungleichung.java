27d346a0358de7bdde4d66e1b686b51f86e08fe3
phillip112 <klappergeste@gmail.com>
2026-09-14 10:07:16 +0200

    Dreiecksungleichung.java + zusatzaufgabe

--- einstiegJava/src/Dreiecksungleichung.java
+++ einstiegJava/src/Dreiecksungleichung.java
@@ -1,10 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Dreiecksungleichung {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Geben sie Seite a ein: ");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String text = br.readLine();
+        double a = Double.parseDouble(text.replace(',', '.'));
+        System.out.println(" Geben sie Seite b ein");
+         text = br.readLine();
+        double b = Double.parseDouble(text.replace(',', '.'));
 
-        double a = 6;
-        double b = 4;
-        double c = 5;
+        System.out.println(" Geben sie Seite c ein");
+         text = br.readLine();
+        double c = Double.parseDouble(text.replace(',', '.'));
+
 
 
         if (a < b + c && b < a + c && c < a + b) {
