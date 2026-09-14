d96f367177ff4b0624513c64148633862c86b0a2
phillip112 <klappergeste@gmail.com>
2026-09-14 14:47:42 +0200

    Summe aller Zahlen + Do while

---
+++ einstiegJava/src/SummeDoWhile.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class SummeDoWhile {
+
+
+        public static void main(String[] args)  throws IOException {
+            System.out.println("Geben sie Ihre Summe ein:");
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String text = br.readLine();
+            double grenze = Double.parseDouble(text.replace(',', '.'));
+
+
+          int i = 0;
+
+          do {
+              System.out.println(i);
+
+              i = i + 1;
+          } while (i >= 100);
+
+          }
+
+            }
+
+
+
