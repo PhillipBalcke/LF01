8b4be851efd81473e52e9798313cb69e8bc33ec3
phillip112 <klappergeste@gmail.com>
2026-09-14 11:05:46 +0200

    Dreiecksungleichung.java + zusatzaufgabe

--- einstiegJava/src/Dreiecksungleichung.java
+++ einstiegJava/src/Dreiecksungleichung.java
@@ -10,27 +10,27 @@ public class Dreiecksungleichung {
         String text = br.readLine();
         double a = Double.parseDouble(text.replace(',', '.'));
         System.out.println(" Geben sie Seite b ein");
-         text = br.readLine();
+        text = br.readLine();
         double b = Double.parseDouble(text.replace(',', '.'));
 
         System.out.println(" Geben sie Seite c ein");
-         text = br.readLine();
+        text = br.readLine();
         double c = Double.parseDouble(text.replace(',', '.'));
 
 
 
         if (a < b + c && b < a + c && c < a + b) {
             System.out.println("Das Zeichnen eines Dreiecks mit den Seitenlängen ist möglich");
+            double s = ( a + c + b) /2;
+            double umfang = (a + b +c);
+            double flaeche = Math.sqrt(s * (s-a) * (s-b) * (s-c));
+            System.out.println("Flächeninhalt eines Dreiecks" + " = " + flaeche);
+            System.out.println("Umfang eines Dreiecks" + " = " + umfang);
         } else {
             System.out.println("Das Zeichnen eines Dreiecks mit den Seitenlängen ist nicht möglich");
         }
 
-        double s = ( a + c + b) /2;
-        double e = (a + b +c);
-        double A = Math.sqrt(s * (s-a) * (s-b) * (s-c));
 
-        System.out.println("Flächeninhalt eines Dreiecks" + " = " + A);
-        System.out.println("Umfang eines Dreiecks" + " = " + e);
 
 
     }
