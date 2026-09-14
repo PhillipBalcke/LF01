5618c2a88c9759c6edf17766787257a706ea7a55
phillip112 <klappergeste@gmail.com>
2026-09-14 09:36:48 +0200

    Dreiecksungleichung.java + zusatzaufgabe

--- einstiegJava/src/Dreiecksungleichung.java
+++ einstiegJava/src/Dreiecksungleichung.java
@@ -14,9 +14,11 @@ public class Dreiecksungleichung {
         }
 
         double s = ( a + c + b) /2;
+        double e = (a + b +c);
         double A = Math.sqrt(s * (s-a) * (s-b) * (s-c));
 
-        System.out.println("Flächeninhalt eines Dreiecks" + A);
+        System.out.println("Flächeninhalt eines Dreiecks" + " = " + A);
+        System.out.println("Umfang eines Dreiecks" + " = " + e);
 
 
     }
